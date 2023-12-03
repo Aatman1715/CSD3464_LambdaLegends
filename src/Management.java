d11da709373762d8ca25197f40f76b1ae4f20733
Karamveer <karamveerthedesign@gmail.com>
2023-12-02 22:07:38 -0500

    karam commit

--- src/Management.java
+++ src/Management.java
@@ -7,45 +7,35 @@ public class Management {
     List<Employee> employees = new ArrayList<>();
 
     // constructor for Motorcycle
-    // make, plate, color, category, sidecar
-    Motorcycle m1 = new Motorcycle("Kawasaki", "Custom Plate", "Yellow", VehicleType.RACE, false);
-    Motorcycle m2 = new Motorcycle("Honda", "Custom Plate", "Black", VehicleType.NOT_FOR_RACE, true);
+    Motorcycle m1 = new Motorcycle("Duckti", "Custom Plate", "Yellow", VehicleType.RACE, false);
+    Motorcycle m2 = new Motorcycle("Yahama", "Custom Plate", "Black", VehicleType.NOT_FOR_RACE, true);
 
     // constructor for Car
-    // make, plate, color, category, gear, type
-    Car vt1 = new Car("Lamborghini", "Custom Plate", "White", VehicleType.Family, Gear.Manual, CarType.Sport);
-    Car vt2 = new Car("BMW", "Custom Plate", "Black", VehicleType.Family, Gear.Automatic, CarType.Sedan);
-    Car vt3 = new Car("Renault Clio", "Custom Plate", "Blue", VehicleType.Family, Gear.Manual, CarType.Hatchback);
-    Car vt4 = new Car("Mazda", "Custom Plate", "White", VehicleType.Family, Gear.Automatic, CarType.SUV);
+    Car vt1 = new Car("Rolls Royce", "Custom Plate", "White", VehicleType.Family, Gear.Manual, CarType.Sport);
+    Car vt2 = new Car("Kia", "Custom Plate", "Green", VehicleType.Family, Gear.Automatic, CarType.Sedan);
+    Car vt3 = new Car("Ford", "Custom Plate", "Blue", VehicleType.Family, Gear.Manual, CarType.Hatchback);
+    Car vt4 = new Car("Ram", "Custom Plate", "Red", VehicleType.Family, Gear.Automatic, CarType.SUV);
 
-    //    Employee constructors
-    // Manager: name, birthYear, nbClients, nbTravelDays, rate
-    // Programmer: name, birthYear, nbProjects, rate
-    // Tester: name, birthYear, nbBugs, rate
     void hireTest1() {
-        employees.add(new Manager("Serge", 1985, 30, 4));
-        employees.add(new Manager("Cindy", 1974, 20, 6));
-        employees.add(new Programmer("Paul", 1993, 3, 75));
-        employees.add(new Tester("Pierre", 1987, 124, 50));
-        employees.add(new Programmer("Matt", 1981, 5, 110));
-        employees.add(new Programmer("Sarah", 1986, 3));
+        employees.add(new Manager("Karam", 1985, 30, 4));
+        employees.add(new Manager("Aatman", 1974, 20, 6));
+        employees.add(new Programmer("Hyder", 1993, 3, 75));
+        employees.add(new Tester("Roseta", 1987, 124, 50));
+        employees.add(new Programmer("Shubu", 1981, 5, 110));
+        employees.add(new Programmer("Kavya", 1986, 3));
         for (Employee employee : employees) {
             System.out.println("We have a new employee: " + employee.getName() + ", a " + employee.getClass().getSimpleName().toLowerCase() + ".");
         }
     }
 
-    //    Employee constructors
-    // Manager: name, birthYear, nbClients, nbTravelDays, rate, employeeVehicle
-    // Programmer: name, birthYear, nbProjects, rate, employeeVehicle
-    // Tester: name, birthYear, nbBugs, rate, employeeVehicle
     void hireTest2() {
         employees.clear();
-        employees.add(new Manager("Serge", 1985, 30, 4, vt1));
-        employees.add(new Manager("Cindy", 1974, 20, 6, 80, vt2));
-        employees.add(new Programmer("Paul", 1993, 3, 75, m1));//1
-        employees.add(new Tester("Pierre", 1987, 124, 50, m2));
-        employees.add(new Programmer("Matt", 1981, 5, 110, vt4));
-        employees.add(new Programmer("Sarah", 1986, 3, vt3));
+        employees.add(new Manager("Karam", 1985, 30, 4, vt1));
+        employees.add(new Manager("Aatman", 1974, 20, 6, 80, vt2));
+        employees.add(new Programmer("Hyder", 1993, 3, 75, m1));//1
+        employees.add(new Tester("Roseta", 1987, 124, 50, m2));
+        employees.add(new Programmer("Shubu", 1981, 5, 110, vt4));
+        employees.add(new Programmer("Kavya", 1986, 3, vt3));
         for (Employee employee : employees) {
             System.out.println("We have a new employee: " + employee.getName() + ", a " + employee.getClass().getSimpleName().toLowerCase() + ".");
         }
@@ -59,15 +49,13 @@ public class Management {
 
     void hireTest3() {
         employees.clear();
-        Employee Serge = new Manager("Serge", 1985, 30, 4, vt1);
-        Employee Cindy = new Manager("Cindy", 1974, 20, 6, 80, vt2);
-        Employee Paul = new Programmer("Paul", 1993, 3, 75, m1);
-        Employee Pierre = new Tester("Pierre", 1987, 124, 50, m2);
-        Employee Matt = new Programmer("Matt", 1981, 5, 110, vt4);
-        Employee Sarah = new Programmer("Sarah", 1986, 3, vt3);
+        Employee Serge = new Manager("Karam", 1985, 30, 4, vt1);
+        Employee Cindy = new Manager("Aatman", 1974, 20, 6, 80, vt2);
+        Employee Paul = new Programmer("Hyder", 1993, 3, 75, m1);
+        Employee Pierre = new Tester("Roseta", 1987, 124, 50, m2);
+        Employee Matt = new Programmer("Shubu", 1981, 5, 110, vt4);
+        Employee Sarah = new Programmer("Kavya", 1986, 3, vt3);
 
-        // Permanent: nbChildren, married, monthlySalary, bonusPerChildPerMonth, accumulatedDays
-        // Temporary: hourlySalary, accumulatedHours
         Contract p1 = new Permanent(2, true, 6000, 200, 17);
         Contract p2 = new Permanent(3, false, 5000, 150, 19);
         Contract t1 = new Temporary(60, 30);
@@ -102,7 +90,7 @@ public class Management {
         }
         System.out.println("-------------------------");
 
-        // updating Matt's contract
+        // update Matt's contract
         Contract p5 = new Permanent(1, true, 5000, 200, 30);
         Matt.signContract(p5);
 
